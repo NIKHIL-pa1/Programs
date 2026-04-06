2b6fa1304ec78d93b4ca2df85648c9a7bf25ee85
NIKHIL-pa1 <143206468+NIKHIL-pa1@users.noreply.github.com>
2026-04-06 10:44:49 +0530

    Add files via upload

---
+++ Alphanumeric String/Alphanumeric String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphanumeric_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputString = ("11603529");
+            int sum = 0;
+            foreach (char c in inputString)
+            {
+                if (char.IsDigit(c))
+                {
+                    sum += int.Parse(c.ToString());
+                }
+            }
+            Console.WriteLine("The sum of digits in the string is: " + sum);
+        }
+    }
+}
